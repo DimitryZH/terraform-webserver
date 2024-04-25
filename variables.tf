25ef6529ef54c974548c98298f735af532986902
Dmitry Zhuravlev <zhdm.dev@gmail.com>
2024-04-25 15:50:00 -0400

    initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "server_name" {
+  description = "WebServer Name"
+  type        = string
+  default     = "demo"
+}
+
+
+variable "server_size" {
+  description = "WebServer Size"
+  type        = string
+  default     = "t2.micro"
+}
